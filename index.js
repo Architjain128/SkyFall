cda074e088b8dfee6baadaa5ba0d7a2cefdcb7a6
Architjain128 <architjain10202@gamil.com>
2021-04-27 19:37:53 +0530

    add hud

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ import {GLTFLoader} from "./lib/GLTFLoader.js"
 import { Vector3 } from "./lib/three.module.js"
 import{OrbitControls} from "./lib/OrbitControls.js"
 var HEALTHBAR = document.getElementById("HEALTHBAR")
+var HEALTHBAR2 = document.getElementById("HEALTHBAR2")
 var HEALTHBARUI = document.getElementById("HEALTHBARUI")
 var HEALTHBARUI2 = document.getElementById("HEALTHBARUI2")
 var SCOREBAR = document.getElementById("SCOREBAR")
+var SCOREBAR2 = document.getElementById("SCOREBAR2")
 var health = 100
 var score = 0
 var GAMEZONE = document.getElementById("GAME")
 var height = GAMEZONE.offsetHeight
 var width = GAMEZONE.offsetWidth
+var STATUS = "INIT"
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
 camera.position.x=0
 camera.position.y=0
 camera.position.z=-500
-const renderer = new THREE.WebGLRenderer({antialias: true})
+const renderer = new THREE.WebGLRenderer({antialias: true,alpha: true})
 renderer.setClearColor("#87ceeb");
 renderer.setSize( width, height )
 GAMEZONE.appendChild( renderer.domElement )
@@ -27,15 +30,26 @@ const Objloader = new THREE.OBJLoader();
 var light = new THREE.HemisphereLight(0xffffff,0x000000,2);
 scene.add(light)
 
-// var backgroundSphere = new THREE.Mesh(
-//     new THREE.SphereGeometry(7000,100,100),
-//     new THREE.MeshBasicMaterial({
-//         map: (new THREE.TextureLoader).load("./public/bg.jpg"),
-//         // side: c.DoubleSide
-//     })
-// );
-// backgroundSphere.position.z=7200
-// scene.add(backgroundSphere)
+var HudCanvas = document.createElement('canvas')
+HudCanvas.width = width
+HudCanvas.height = height
+var HudBitmap = HudCanvas.getContext('2d')
+HudBitmap.font = "Normal 40px Arial";
+HudBitmap.textAlign = 'center';
+HudBitmap.fillStyle = "rgba(245,245,245,0.75)";
+HudBitmap.fillText('Initializing...',width/2, height/2);
+var cameraHUD = new THREE.OrthographicCamera(-width/2, width/2, height/2, -height/2, 0, 30 )
+var sceneHUD = new THREE.Scene();
+var HudTexture = new THREE.Texture(HudCanvas) 
+HudTexture.needsUpdate = true;
+var material = new THREE.MeshBasicMaterial( {map: HudTexture} );
+material.transparent = true;
+
+var planeGeometry = new THREE.PlaneGeometry( width, height );
+var plane = new THREE.Mesh( planeGeometry, material );
+sceneHUD.add( plane );
+console.log("hu",plane)
+
 
 var bgWidth
 var bgHeight
@@ -62,11 +76,9 @@ var Player31
 var Player32
 var Player33
 var Player34
-
 var Player1_Tex1
 var Player2_Tex1
 var Player1_Tex2
-
 var pltex = Texloader.load(
     "../odel/player/Jet_by_dommk/textures/jet_by_dommk_Base_Color.png",
     function(plt){
@@ -85,7 +97,6 @@ var pltex = Texloader.load(
         Player2_Tex1=plt 
     },
 )
-
 var pl = Objloader.load(
     "../odel/player/Jet_by_dommk/model/jet_by_dommk.obj",
     function(player){
@@ -95,7 +106,6 @@ var pl = Objloader.load(
         player.rotation['x']= -1.57
         player.rotation['y']= -1.57
         player.position['y']= 300
-        // player.material.map=Player_Tex
         player.children[0].material.map=Player1_Tex1
         Player1=player
         scene.add(Player1)
@@ -107,7 +117,6 @@ var pl = Objloader.load(
 		console.log( 'An error happened in player' );
 	}
 )
-
 var pl2 = Objloader.load(
     "../odel/Feisar_Ship_OBJ/Feisar_Ship.obj",
     function(player){
@@ -127,7 +136,6 @@ var pl2 = Objloader.load(
 		console.log( 'An error happened in player' );
 	}
 )
-
 var pl31 = Objloader.load(
     "../odel/op.obj",
     function(player){
@@ -151,7 +159,6 @@ var pl31 = Objloader.load(
 		console.log( 'An error happened in player' );
 	}
 )
-
 var pl32 = Objloader.load(
     "../odel/op.obj",
     function(player){
@@ -175,7 +182,6 @@ var pl32 = Objloader.load(
 		console.log( 'An error happened in player' );
 	}
 )
-
 var pl33 = Objloader.load(
     "../odel/op.obj",
     function(player){
@@ -199,7 +205,6 @@ var pl33 = Objloader.load(
 		console.log( 'An error happened in player' );
 	}
 )
-
 var pl34 = Objloader.load(
     "../odel/op.obj",
     function(player){
@@ -223,6 +228,7 @@ var pl34 = Objloader.load(
 		console.log( 'An error happened in player' );
 	}
 )
+
 var gridHelper = new THREE.GridHelper( 1000, 10);
 gridHelper.rotation['x']=1.57
 console.log(gridHelper.rotation)
@@ -237,7 +243,6 @@ var controls = new OrbitControls(camera, renderer.domElement)
 controls.update();
 
 render()
-
 
 document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown(event) {
@@ -266,6 +271,7 @@ function onDocumentKeyDown(event) {
 function random_value(a){
     return Math.random()*2*a -a
 }
+
 function random(){
     var a = 2
     Player31.position['x'] = Player31.position['x'] + random_value(a)
@@ -282,32 +288,48 @@ function render() {
     requestAnimationFrame(render)
     controls.update()
     renderer.render( scene, camera )
+    if(STATUS==="GAMEOVER"){
+        document.getElementById("HUD").style.display="none"
+        renderer.render(sceneHUD, cameraHUD); 
+    }
+    Hudupdate()
     scorify()
     healthify()
     // random()
 }
 
+function Hudupdate(){
+    HudBitmap.clearRect(0, 0, width, height);
+    HudBitmap.fillText("GAME OVER" ,width/2, height / 2);
+    HudBitmap.fillText("Score : "+score , width / 2, (2* height) / 3);
+    HudTexture.needsUpdate = true;
+    material = new THREE.MeshBasicMaterial( {map: HudTexture} );
+    material.transparent = true;
+}
+
 function scorify(){
     SCOREBAR.innerHTML=score
+    SCOREBAR2.innerHTML=score
 }
 
 function healthify(){
     if(health>70 && health <= 100)
     {
         HEALTHBARUI.style.backgroundColor="green"
-        // HEALTHBAR.style.color="green"
         HEALTHBARUI2.style.height = ((100-health)+"%").toString()
     }else if(health>30 && health <= 70){
         HEALTHBARUI.style.backgroundColor="orange"
-        // HEALTHBAR.style.color="orange"
         HEALTHBARUI2.style.height = ((100-health)+"%").toString()
     }else
     {
         HEALTHBARUI.style.backgroundColor="red"
-        // HEALTHBAR.style.color="orange"
         HEALTHBARUI2.style.height = ((100-health)+"%").toString()
     }
     HEALTHBAR.innerHTML=health
+    HEALTHBAR2.innerHTML=health
+    if(health==0){
+        STATUS="GAMEOVER"
+    }
 }
 
 function resize(){
